7a852c068f99046335d83f053b645ef9611c2b97
Juan Pleitez <juanpc13lolol@gmail.com>
2019-09-24 21:11:28 -0600

    Agregando Servidor y Weapp vacia y validacion de placa

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <Arduino.h>
 #include <ESPAsyncWebServer.h>
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <SPIFFS.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include "FS.h"
+#endif
 
 const char *ssid = "Ceia2019";
 const char *password = "87654321";
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
 void setup() {
   //Puerto Serial
@@ -14,6 +26,12 @@ void setup() {
   }
   //Mostrar IP
   Serial.print("IP : ");Serial.println(WiFi.localIP());
+  //Iniciando la memoria SPIFFS
+  SPIFFS.begin();
+  //Servidor de Archivos de la memoria SPIFFS
+  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+  //Iniciar Servidor
+  server.begin();
 }
 
 void loop() {
