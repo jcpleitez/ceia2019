041c29394df8b94aa630dd8445cb16e075c0e3cd
Juan Pleitez <juanpc13lolol@gmail.com>
2019-09-24 20:34:59 -0600

    Conexion al Wifi y agregando otra placa

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+
+const char *ssid = "Ceia2019";
+const char *password = "87654321";
 
 void setup() {
-  // put your setup code here, to run once:
+  //Puerto Serial
+  Serial.begin(115200);Serial.println();
+  //Conectar Wifi
+  WiFi.mode(WIFI_STA);WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");delay(100);
+  }
+  //Mostrar IP
+  Serial.print("IP : ");Serial.println(WiFi.localIP());
 }
 
 void loop() {
